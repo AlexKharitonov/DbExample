04d8a552228fcb3a21ce58c34d8874fd5b15d1e0
alexk <alex117hariton@gmail.com>
2025-09-13 07:35:37 +0500

    init

---
+++ DbExample/src/main/java/ak/teaching/dbexample/DbExampleApplication.java
@@ -0,0 +1,13 @@
+package ak.teaching.dbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbExampleApplication.class, args);
+    }
+
+}
---
+++ DbExample/src/test/java/ak/teaching/dbexample/DbExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ak.teaching.dbexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
